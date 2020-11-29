378561328ecc704c2c8f60bc7a6e46642f50e5be
Jakub Chmielewski <jakub.jacek.chmielewski@gmail.com>
2020-11-29 19:12:33 +0100

    basic task finished

--- scripts/script.js
+++ scripts/script.js
@@ -9,8 +9,11 @@ hasChildren.forEach((e)=>{
 let closeResMenu = document.querySelector('.burger.closer')
 closeResMenu.addEventListener('click', ()=>{
     document.querySelector('.wrapper-bottom__content').style.right = '-100%'
+    document.querySelector('body').style.overflow = ''
 })
 let openeResMenu = document.querySelector('.burger.opener')
 openeResMenu.addEventListener('click', ()=>{
     document.querySelector('.wrapper-bottom__content').style.right = '0'
+    document.querySelector('body').style.overflow = 'hidden'
+
 })
