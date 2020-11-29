b23bceb2bf447fc7ffea66921500c4a62b240751
Jakub Chmielewski <jakub.jacek.chmielewski@gmail.com>
2020-11-29 21:18:11 +0100

    extra task - contact box modal

--- scripts/script.js
+++ scripts/script.js
@@ -17,3 +17,31 @@ openeResMenu.addEventListener('click', ()=>{
     document.querySelector('body').style.overflow = 'hidden'
 
 })
+let dimmer = document.querySelector('.dimmer')
+let modal = document.querySelector('.modal')
+const closeModal = document.querySelector('.close-modal')
+closeModal.addEventListener('click', ()=>{
+    let modalBoxContact = document.querySelector('.modal-box-contact')
+    modalBoxContact.style.opacity = '0';
+    dimmer.style.opacity = '0';
+    setTimeout(()=>{
+        modal.style.display = 'none'
+    },300);
+
+})
+
+const openModal = document.querySelector('.open-modal')
+openModal.addEventListener('click', ()=>{
+    let modalBoxContact = document.querySelector('.modal-box-contact')
+    var opacity = 0; 
+    modal.style.display = 'flex'
+    var intervalID = setInterval(function() { 
+        if (opacity < 1) { 
+            opacity = opacity + 0.1 
+            modalBoxContact.style.opacity = opacity; 
+            dimmer.style.opacity = opacity; 
+        } else { 
+            clearInterval(intervalID); 
+        } 
+    }, 20); 
+})
